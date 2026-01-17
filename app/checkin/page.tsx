ee165212b7493ef0330df4fea04fb5ab4ab3a06c
Hilaire Bouyé <hilairebouye@Hilaires-MacBook-Pro.local>
2026-01-17 19:20:47 +0100

    Fix supabaseClient path for Vercel

--- app/checkin/page.tsx
+++ app/checkin/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { supabase } from "../../lib/supabaseClient";
+import { supabase } from "../lib/supabaseClient";
+
 
 function mondayOfThisWeek(d = new Date()) {
   const date = new Date(d);
